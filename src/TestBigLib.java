1a33e402e2cd42954c704624bf0053bafd8e4d3f
Faeq Alrimawi <faeq.rimawi@gmail.com>
2018-02-15 15:52:57 +0000

    c

---
+++ src/TestBigLib.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+
+import it.uniud.mads.jlibbig.core.std.*;
+
+
+public class TestBigLib {
+	
+	public static void main(String[] args) {
+		Control room = new Control("Room", true, 1);
+		Control room2 = new Control("Hallway", true, 1);
+		LinkedList<Control> ctrls = new LinkedList<Control>();
+		ctrls.add(room);
+		ctrls.add(room2);
+		Signature sig = new Signature(ctrls);
+		
+		BigraphBuilder bi = new BigraphBuilder(sig);
+		OuterName a1 = bi.addOuterName("p1");
+		OuterName a2 = bi.addOuterName("p2");
+		Root root = bi.addRoot(0);
+		Root root1 = bi.addRoot(1);
+		Node r2 = bi.addNode("Room", root1, a1);
+		Node r3 = bi.addNode("Hallway", root1, a1);
+		Bigraph p = bi.makeBigraph();
+		
+		System.out.println(p.toString());
+		//System.out.println(p.getSignature().toString());
+	
+		
+	}
+
+}
