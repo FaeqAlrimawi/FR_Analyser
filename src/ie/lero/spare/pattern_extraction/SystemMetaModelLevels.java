f950cf3b6c93d336512ebbd682ac5435fd922ab7
Faeq Alrimawi <faeq.rimawi@gmail.com>
2018-04-02 23:02:52 +0900

    a

---
+++ src/ie/lero/spare/pattern_extraction/SystemMetaModelLevels.java
@@ -0,0 +1,13 @@
+package ie.lero.spare.pattern_extraction;
+
+
+
+public class SystemMetaModelLevels {
+	
+	public  static void main(String [] args) {
+		
+		ExtendedMetaData modelMetaData = new BasicExtendedMetaData(myResourceSet.getPackageRegistry());
+		
+	}
+
+}
