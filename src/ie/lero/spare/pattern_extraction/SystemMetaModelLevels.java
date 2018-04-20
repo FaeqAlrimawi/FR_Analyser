a4c2b5cf38920214e62595a17868d188629be428
Faeq Alrimawi <faeq.rimawi@gmail.com>
2018-04-20 19:24:27 +0900

    a

--- src/ie/lero/spare/pattern_extraction/SystemMetaModelLevels.java
+++ src/ie/lero/spare/pattern_extraction/SystemMetaModelLevels.java
@@ -1,12 +1,21 @@
 package ie.lero.spare.pattern_extraction;
 
-
+import environment.Building;
+import environment.EnvironmentDiagram;
+import environment.HVAC;
+import environment.Status;
+import environment.smartbuildingFactory;
+import environment.impl.HVACImpl;
 
 public class SystemMetaModelLevels {
 	
 	public  static void main(String [] args) {
 		
 		//ExtendedMetaData modelMetaData = new BasicExtendedMetaData(myResourceSet.getPackageRegistry());
+		HVAC s = smartbuildingFactory.eINSTANCE.createHVAC();
+		s.setStatus(Status.OFF);
+		System.out.println(s.getStatus()+" "+s.isAbstractable());
+		
 		
 		
 	}
